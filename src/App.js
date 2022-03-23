cbeee3abf7cd3a24bd881e94015e5a524c619122
Denver McCarthy <dmichaelspdx@gmail.com>
2022-03-23 10:53:14 -0700

    added link to list from homepg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Book from './components/book/Book';
+import { Link } from 'react-router-dom';
+import Home from './views/Books/Home';
 
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <BrowserRouter>
+      <main className="container">
+        <h1>Library Catalog</h1>
+      </main>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/list">
+          <BookList />
+        </Route>
+        <Route exact path="/list/:id">
+          <Book />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/views/Books/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Welcome to Dangalf`s Library of Computer Science!</h2>
+      <h3>
+        Check out the amazing selection of books in our <Link to={'/list'}>catalog.</Link>
+      </h3>
+    </div>
+  );
+}
